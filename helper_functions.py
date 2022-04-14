7277d88fa62bfc93ccddb023009cdc2502f8edd5
tak3369 <98980668+tak3369@users.noreply.github.com>
2022-04-14 16:38:35 -0700

    comments added, function names changed for clarity, aws methods have been modified to account for school AWS account

--- helper_functions.py
+++ helper_functions.py
@@ -4,54 +4,66 @@ from database import *
 from flask_login import UserMixin
 
 
-# Create user associated with patient
-def create_language(name,prefix):
-    language_entry = Language(name=name,prefix=prefix)
+def create_language(name, prefix):
+    """Prefix should be the first three letters of the language (lowercase)."""
+    language_entry = Language(name=name,
+                              prefix=prefix)
     db.session.add(language_entry)
     db.session.commit()
     return language_entry
 
 
-def create_list_group(promptId,groupId):
-    prompt = db.session.query(Prompt).filter(Prompt.promptId==promptId).first()
-    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.groupOfPromptId==groupId).first()
-    list_group_entry = ListGroup(groupOfPrompt =group.groupOfPromptId,prompt=prompt.promptId)
+def create_list_group(prompt_id, group_id):
+    prompt = db.session.query(Prompt).filter(Prompt.promptId == prompt_id).first()
+    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.groupOfPromptId == group_id).first()
+    list_group_entry = ListGroup(groupOfPrompt=group.groupOfPromptId,
+                                 prompt=prompt.promptId)
     db.session.add(list_group_entry)
     db.session.commit()
-    return  list_group_entry
+    return list_group_entry
 
 
-def create_type_of_media(name,extension):
-    tom_entry = TypeOfMedia(nameMedia=name,extension=extension)
+def create_type_of_media(name, extension):
+    tom_entry = TypeOfMedia(nameMedia=name,
+                            extension=extension)
     db.session.add(tom_entry)
     db.session.commit()
     return tom_entry
 
 
-def create_patient(username, password, firstName, lastName, languageId,e, expertId, dob, sex):
-    username_exists = db.session.query(User).filter(User.username==username).first() is not None
+def create_patient(username, password, first_name, last_name, language_id, e, expert_id, dob, sex):
+    username_exists = db.session.query(User).filter(User.username == username).first() is not None
     if username_exists:
         print("Sorry, username is taken")
     else:
-        user_entry = User(username=username,password=password)
+        user_entry = User(username=username,
+                          password=password)
         # get language
-        language = db.session.query(Language).filter(Language.languageId == languageId).first()
+        language = db.session.query(Language).filter(Language.languageId == language_id).first()
         # get expert
-        expert = db.session.query(Expert).filter(Expert.expertId == expertId).first()
-        patient_entry = Patient(firstName = firstName, lastName=lastName, language=language, e=e, expertId=expert.expertId, dob=dob, sex=sex, user=user_entry)
+        expert = db.session.query(Expert).filter(Expert.expertId == expert_id).first()
+        patient_entry = Patient(firstName=first_name,
+                                lastName=last_name,
+                                language=language,
+                                e=e,
+                                expertId=expert.expertId,
+                                dob=dob,
+                                sex=sex,
+                                user=user_entry)
         db.session.add(patient_entry)
         db.session.commit()
         return patient_entry
 
 
-def create_image(name, filePath):
-    image_entry = Image(filePath=filePath, name=name)
+def create_image(name, file_path):
+    image_entry = Image(filePath=file_path,
+                        name=name)
     db.session.add(image_entry)
     db.session.commit()
     return image_entry
 
 
-def create_group_of_prompt(name): # group of prompts
+def create_group_of_prompt(name):  # group of prompts
     gop_entry = GroupOfPrompt(name=name)
     db.session.add(gop_entry)
     db.session.commit()
@@ -59,19 +71,25 @@ def create_group_of_prompt(name): # group of prompts
 
 
 def create_type_of_prompt(name, description):
-    top_entry  = TypeOfPrompt(name=name,description=description)
+    top_entry = TypeOfPrompt(name=name,
+                             description=description)
     db.session.add(top_entry)
     db.session.commit()
     return top_entry
 
 
-def create_expert(username, password, firstName, lastName, licenseNumber, e):
+def create_expert(username, password, first_name, last_name, license_number, e):
     username_exists = db.session.query(User).filter(User.username == username).first() is not None
     if username_exists:
         print("Sorry, username is taken")
     else:
-        user_entry = User(username=username, password=password)
-        expert_entry = Expert(firstName=firstName, lastName=lastName,licenseNumber=licenseNumber,e=e,user=user_entry)
+        user_entry = User(username=username,
+                          password=password)
+        expert_entry = Expert(firstName=first_name,
+                              lastName=last_name,
+                              licenseNumber=license_number,
+                              e=e,
+                              user=user_entry)
         db.session.add(expert_entry)
         db.session.commit()
         return expert_entry
@@ -81,7 +99,7 @@ def create_expert(username, password, firstName, lastName, licenseNumber, e):
 def get_user_by_name(username):
     user = db.session.query(User).filter(User.username == username).first()
     print(user)
-    dict = {'username':user.username,'password':user.password}
+    dict = {'username': user.username, 'password': user.password}
     if user is not None:
         return user
     else:
@@ -89,25 +107,25 @@ def get_user_by_name(username):
         return -1
 
 
-# get user by userId (used for login user_loader)
 def get_user_by_id(user_id):
-    user = db.session.query(User).filter(User.userId== user_id).first()
+    """Used for login user_loader."""
+    user = db.session.query(User).filter(User.userId == user_id).first()
     if user is not None:
         return user
     else:
         return None
 
 
-def add_prompt_to_group(groupName,prompt):
+def add_prompt_to_group(group_name, prompt):
     # get the group
-    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.name==groupName).first()
+    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.name == group_name).first()
     # get the prompt
-    prompt = db.session.query(Prompt).filter(Prompt.promptId==prompt).first()
+    prompt = db.session.query(Prompt).filter(Prompt.promptId == prompt).first()
     last_id = get_list_group_id()
     list_group = ListGroup()
-    list_group.listGroupId= last_id+1
-    list_group.groupOfPromptId=group.groupOfPromptId
-    list_group.promptId=prompt.promptId
+    list_group.listGroupId = last_id+1
+    list_group.groupOfPromptId = group.groupOfPromptId
+    list_group.promptId = prompt.promptId
 
     prompt.groupOfPrompt.append(list_group)
     db.session.commit()
@@ -116,34 +134,43 @@ def add_prompt_to_group(groupName,prompt):
 
 
 def get_list_group_id():
-    id = db.session.query(ListGroup).order_by(ListGroup.listGroupId.desc()).first()
+    list_group_id = db.session.query(ListGroup).order_by(ListGroup.listGroupId.desc()).first()
     #print(id.listGroupId)
-    return id.listGroupId
+    return list_group_id.listGroupId
 
 
-def create_asg(group_id,date_of_asg,sop,expertN,expert_id,patient_id):
+def create_asg(group_id, date_of_asg, sop, expert_note, expert_id, patient_id):
     # get expert
-    expert = db.session.query(Expert).filter(Expert.expertId==expert_id).first()
+    expert = db.session.query(Expert).filter(Expert.expertId == expert_id).first()
     # get patient
-    patient = db.session.query(Patient).filter(Patient.patientId==patient_id).first()
+    patient = db.session.query(Patient).filter(Patient.patientId == patient_id).first()
     #get group of Prompts
-    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.groupOfPromptId==group_id).first()
+    group = db.session.query(GroupOfPrompt).filter(GroupOfPrompt.groupOfPromptId == group_id).first()
 
-    asg_entry = Assignment(groupOfPromptsId=group.groupOfPromptId,dateOfAssignment=date_of_asg,stateOfPrompt=sop,expertNote=expertN,expertId=expert.expertId,patientId=patient.patientId)
+    asg_entry = Assignment(groupOfPromptsId=group.groupOfPromptId,
+                           dateOfAssignment=date_of_asg,
+                           stateOfPrompt=sop,
+                           expertNote=expert_note,
+                           expertId=expert.expertId,
+                           patientId=patient.patientId)
     db.session.add(asg_entry)
     db.session.commit()
     print(asg_entry)
     return asg_entry
 
 
-def create_media(media_note,file_path,asg,prompt,type_media):
+def create_media(media_note, file_path, asg, prompt, type_media):
     # get assignment
-    asg_id = db.session.query(Assignment).filter(Assignment.assignmentId==asg).first()
+    asg_id = db.session.query(Assignment).filter(Assignment.assignmentId == asg).first()
     # get prompt
-    prompt_id = db.session.query(Prompt).filter(Prompt.promptId==prompt).first()
+    prompt_id = db.session.query(Prompt).filter(Prompt.promptId == prompt).first()
     # get type of media
-    type_media_id = db.session.query(TypeOfMedia).filter(TypeOfMedia.typeOfMediaId==type_media).first()
-    media_entry = Media(mediaNote=media_note,filePath=file_path,assignmentId=asg_id.assignmentId,promptId =prompt_id.promptId, typeOfMediaId=type_media_id.typeOfMediaId)
+    type_media_id = db.session.query(TypeOfMedia).filter(TypeOfMedia.typeOfMediaId == type_media).first()
+    media_entry = Media(mediaNote=media_note,
+                        filePath=file_path,
+                        assignmentId=asg_id.assignmentId,
+                        promptId=prompt_id.promptId,
+                        typeOfMediaId=type_media_id.typeOfMediaId)
     db.session.add(media_entry)
     db.session.commit()
     print(media_entry)
@@ -151,13 +178,13 @@ def create_media(media_note,file_path,asg,prompt,type_media):
 
 
 def get_expert(username):
-    user_name = db.session.query(User).filter(User.username==username).first()
+    user_name = db.session.query(User).filter(User.username == username).first()
     if user_name is not None:
-        #user exists, therefore we can get id
+        # user exists, therefore we can get id
         user_id = user_name.userId
         # get email
-        e = db.session.query(Expert).filter(Expert.userId==user_id).first()
-        print("email: ",e.e)
+        e = db.session.query(Expert).filter(Expert.userId == user_id).first()
+        print("email: ", e.e)
         print("User id is:", user_id)
     else:
         print("Username does not exist")
@@ -168,11 +195,13 @@ def get_expert(username):
 
 # get all media files for a given patient
 def get_media_file(username):
-    user_name = db.session.query(User).filter(User.username==username).first()
-    patient = db.session.query(Patient).filter(Patient.userId==user_name.userId).first() # get patient
-    #print(patient.patientId)
-    media_file = db.session.query(Media,Assignment).join(Assignment,Assignment.assignmentId==Media.assignmentId).filter(Assignment.patientId==patient.patientId).all()
-    print(media_file)
+    user_name = db.session.query(User).filter(User.username == username).first()
+    patient = db.session.query(Patient).filter(Patient.userId == user_name.userId).first()  # get patient
+    # print(patient.patientId)
+    media_file = db.session.query(Media, Assignment)
+    media_file.join(Assignment, Assignment.assignmentId == Media.assignmentId)
+    media_file.filter(Assignment.patientId==patient.patientId).all()
+    # print(media_file)
     for m, asg in media_file:
         print("Media Id:{} MediaNote: {} filePath: {} Media.assignmentId:{} promptId: {} typeOfMediaId: {} Assignments.assignmetId: {} dateOfAssignment:{} stateOfPrompt:{} expertNote:{} groupOfPromptsId: {} expertId: {} patientId: {}"
         .format(m.mediaId,m.mediaNote,m.filePath,m.assignmentId,m.promptId,m.typeOfMediaId,asg.assignmentId,asg.dateOfAssignment,asg.stateOfPrompt,asg.expertNote,asg.groupOfPromptsId,asg.expertId,asg.patientId) )
--- prompt_helper_functions.py
+++ prompt_helper_functions.py
@@ -1,81 +1,113 @@
 from database import *
 from flask_login import current_user
 
+
 def get_all_prompts():
-    control=0
-    prompts_to_return=[]
+    control = 0
+    prompts_to_return = []
     prompts = db.session.query(TypeOfPrompt).all()
     for i in prompts:
-        promt_data = {'type_of_prompt':prompts[control].typeOfPromptId,
-                      'prompt_name':prompts[control].name}
+        prompt_data = {'type_of_prompt': prompts[control].typeOfPromptId,
+                       'prompt_name': prompts[control].name}
         control += 1
-        prompts_to_return.append(promt_data)
+        prompts_to_return.append(prompt_data)
 
     get_prompt = json.dumps(prompts_to_return)
     print(get_prompt)
     print(prompts_to_return)
     return prompts_to_return
 
-def getExpert():
-    expert=0
+
+def get_expert():
+    expert = 0
     if current_user.is_authenticated:
         expert = current_user.get_id()
         return expert
     return
+
 
 def get_languages():
     control = 0
     languages_to_return = []
     languages = db.session.query(Language).all()
     for i in languages:
-        languages_data= {'language_id':languages[control].languageId,'name':languages[control].name,'prefix':languages[control].prefix}
+        languages_data = {'language_id': languages[control].languageId,
+                          'name': languages[control].name,
+                          'prefix': languages[control].prefix}
         control += 1
         languages_to_return.append(languages_data)
-    print(languages_to_return)
+    # print(languages_to_return)
 
     return languages_to_return
 
-def create_prompt(description, languageId, expertId, imageId, topId):
-    # create object of group_of_prompt
 
-    # get the type pf prompt id
-    type_of_prompt_id = db.session.query(TypeOfPrompt).filter(TypeOfPrompt.typeOfPromptId == topId).first()
-    # get the language
-    language_id = db.session.query(Language).filter(Language.languageId == languageId).first()
-    # get the expert
-    expert_id = db.session.query(Expert).filter(Expert.expertId == expertId).first()
-    # get the image
-    image_id = db.session.query(Image).filter(Image.imageId == imageId).first()
+def add_prompt_to_database(description, given_language_id, given_expert_id, given_image_id, prompt_type_id):
+    """Creates new prompt and saves it to the database. Returns newly created Prompt object.
 
-    prompt_entry = Prompt(descriptionPrompt=description, languageId=language_id.languageId, expertId=expert_id.expertId,
-                          imageId=image_id.imageId, typeOfPromptId=type_of_prompt_id.typeOfPromptId)
-    db.session.add(prompt_entry)
+    Arguments:
+        description: Prompt text. Make it pretty! *Will be displayed to the patient!*
+        given_language_id: Language ID associated with prompt.
+        given_expert_id: Expert ID associated with prompt.
+        given_image_id: Image ID associated with prompt.
+        prompt_type_id: ID of the prompt type. Corresponds to either Spontaneous, Semi-Spon, or Non-Spon.
+    """
+
+    # Get prompt type
+    type_of_prompt_id = db.session.query(TypeOfPrompt).filter(TypeOfPrompt.typeOfPromptId == prompt_type_id).first()
+    # Get language
+    language_id = db.session.query(Language).filter(Language.languageId == given_language_id).first()
+    # Get expert
+    expert_id = db.session.query(Expert).filter(Expert.expertId == given_expert_id).first()
+    # Get image
+    image_id = db.session.query(Image).filter(Image.imageId == given_image_id).first()
+
+    new_prompt_entry = Prompt(descriptionPrompt=description,
+                              languageId=language_id.languageId,
+                              expertId=expert_id.expertId,
+                              imageId=image_id.imageId,
+                              typeOfPromptId=type_of_prompt_id.typeOfPromptId)
+
+    db.session.add(new_prompt_entry)
     db.session.commit()
-    return prompt_entry
 
-def get_image_id(imageName):
-    if type(imageName)== int:
-        image_id = db.session.query(Image).filter(Image.imageId==imageName).first()
+    return new_prompt_entry
 
 
-    if type(imageName)==str:
-        image_id = db.session.query(Image).filter(Image.name==imageName).first()
+def get_image_id(image_name):
+    if type(image_name) == int:
+        image_id = db.session.query(Image).filter(Image.imageId == image_name).first()
+    elif type(image_name) == str:
+        image_id = db.session.query(Image).filter(Image.name == image_name).first()
 
     return image_id.imageId
 
-def get_language_id(languageId):
-    language_id = db.session.query(Language).filter(Language.languageId==languageId).first()
+
+def get_language_id(given_id):
+    language_id = db.session.query(Language).filter(Language.languageId == given_id).first()
     return language_id.languageId
 
-def get_type_prompt_id(typeId):
-    type_prompt = db.session.query(TypeOfPrompt).filter(TypeOfPrompt.typeOfPromptId==typeId).first()
+
+def get_type_prompt_id(given_id):
+    type_prompt = db.session.query(TypeOfPrompt).filter(TypeOfPrompt.typeOfPromptId == given_id).first()
     return type_prompt.typeOfPromptId
 
-def prompt_creation(prompt_request):
+
+def generate_prompt_from_request(prompt_request):
+    """Parses data from arg: prompt_request (a .json file) and uses parsed data to call the final
+    prompt generation function, add_prompt_to_database. Once added, it returns the newly generated
+    Prompt object.
+
+    Arguments:
+        prompt_request: A .json file consisting of user-submitted prompt data.
+    """
     image_id = get_image_id(prompt_request["image_name"])
-    expert= getExpert()
+    expert = get_expert()
     language = get_language_id(prompt_request["type_of_language"])
     type_of_prompt = get_type_prompt_id(prompt_request["type_of_prompt"])
-    prompt_created = create_prompt(description=prompt_request["prompt_description"],languageId=language,expertId=expert,imageId=image_id,topId=type_of_prompt)
+    prompt_created = add_prompt_to_database(description=prompt_request["prompt_description"],
+                                            given_language_id=language,
+                                            given_expert_id=expert,
+                                            given_image_id=image_id,
+                                            prompt_type_id=type_of_prompt)
     print(prompt_created)
     return prompt_created
