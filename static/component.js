ab9e95ffe1307cac8a520da4e87cba801d13b001
Betsy <b_avila@outlook.com>
2022-04-27 12:55:43 -0700

    expert portal only had two catgories from menu now

--- static/component.js
+++ static/component.js
@@ -69,31 +69,13 @@ class ExpertMenu extends HTMLElement{
                             </a>
                             <h3 class="icon-titles">Prompts</h3>  
                         </li>
-                        <li class="menu-list-item">
-                            <a class="menu-icon" href="/logout">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1A1B41" class="bi bi-translate" viewBox="0 0 16 16">
-                                    <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z"/>
-                                    <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z"/>
-                                </svg> 
-                            </a>
-                            <h3 class="icon-titles">Languages</h3>
-                        </li>
                         <li class="menu-list-item"> 
-                            <a class="menu-icon" href="">
+                            <a class="menu-icon" href="/">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1A1B41" class="bi bi-briefcase" viewBox="0 0 16 16">
                                     <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z"/>
                                 </svg>
                             </a>
                             <h3 class="icon-titles">Patients</h3>
-                        </li>
-                        <li class="menu-list-item">
-                            <a class="menu-icon" href="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1A1B41" class="bi bi-download" viewBox="0 0 16 16">
-                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                                </svg>  
-                            </a>
-                            <h3 class="icon-titles">Downloads</h3>
                         </li>
                     </ul>
                 </div>
