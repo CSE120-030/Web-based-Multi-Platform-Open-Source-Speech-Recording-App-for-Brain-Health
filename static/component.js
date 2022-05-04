2b23bf5a38e99f77c4b977adf4cb26f26c7e08e5
Betsy <b_avila@outlook.com>
2022-05-04 13:48:16 -0700

    adjusted icons

--- static/component.js
+++ static/component.js
@@ -86,14 +86,13 @@ class ExpertMenu extends HTMLElement{
                                 <h3 class="icon-titles">Patients</h3>
                             </a>
                         </li>
-                         <li class="menu-list-item"> 
+                        <li class="menu-list-item">
                             <a class="menu-icon" href="/expertPortal/create_asg">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1A1B41" class="bi bi-briefcase" viewBox="0 0 16 16">
-                                    <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1A1B41" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
                             </a>
-                           
-                          <!-- <a href=" http://127.0.0.1:5000/expertPortal/list_patients" style="text-decoration:none;">-->
                             <a href="/expertPortal/create_asg" style="text-decoration:none;">
                                 <h3 class="icon-titles">Create Assignment</h3>
                             </a>
@@ -155,4 +154,4 @@ class PatientMenu extends HTMLElement{
 
 customElements.define("app-navbar", Navbar);
 customElements.define("expert-menu", ExpertMenu);
-customElements.define("patient
+customElements.define("patient", PatientMenu);
